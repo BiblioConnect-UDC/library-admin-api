52d661ae27787e5b7df4f1791ff8fb359828925b
Manuel Hernández <mhernandezr6@unicartagena.edu.co>
2026-06-04 18:25:48 -0500

    Set up Spring Boot API initial project
    Configures the foundational `library-admin-api` project. This includes the Maven wrapper for consistent builds, core Spring Boot dependencies (Web, JPA, Validation), MySQL connector, Lombok, and initial application structure for Java 21.

---
+++ src/main/java/co/edu/unicartagena/library_admin_api/LibraryAdminApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicartagena.library_admin_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAdminApiApplication.class, args);
+	}
+
+}
